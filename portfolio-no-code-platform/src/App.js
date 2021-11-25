73a7c534d3ef193002f398b3d6d78f2dd1730718
Rick Beyer <beyer.richard73@gmail.com>
2021-11-24 18:11:42 -0600

    setting up main screen

---
+++ portfolio-no-code-platform/src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+import "./styles/styles.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio-no-code-platform/src/components/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-logo'>No-Code Portfolio</div>
+            <nav>
+                <ul>
+                    <li>Get Started</li>
+                    <li>How It Works</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    )
+};
+
+export default Header;
