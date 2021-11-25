849156506a759bf3cbe62b83f8aa62c57cc0c0a8
Rick Beyer <beyer.richard73@gmail.com>
2021-11-24 18:24:56 -0600

    Header setup and responsive

--- portfolio-no-code-platform/src/App.js
+++ portfolio-no-code-platform/src/App.js
@@ -1,10 +1,23 @@
 import Header from './components/Header';
 import "./styles/styles.css";
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResizeWindow = () => setWidth(window.innerWidth);
+
+    window.addEventListener('resize', handleResizeWindow);
+    return () => {
+      window.removeEventListener('resize', handleResizeWindow);
+    };
+
+  }, []);
+
   return (
     <div className="App">
-      <Header />
+      <Header appWidth={width}/>
     </div>
   );
 }
--- portfolio-no-code-platform/src/components/Header.js
+++ portfolio-no-code-platform/src/components/Header.js
@@ -1,13 +1,16 @@
-const Header = () => {
+const Header = (props) => {
     return (
+        
         <div className='header'>
             <div className='header-logo'>No-Code Portfolio</div>
             <nav>
+                {props.appWidth >= 1070 ? 
                 <ul>
                     <li>Get Started</li>
                     <li>How It Works</li>
                     <li>Contact</li>
                 </ul>
+                : null}
             </nav>
         </div>
     )
